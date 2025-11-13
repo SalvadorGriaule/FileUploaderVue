8a51c4a9a40e0ef2e28ef8177a48abaa4817dbee
SalvadorGriaule <morales.quentin@gmx.fr>
2025-11-13 13:38:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FileUploader from "./components/FileUploader.vue"
+</script>
+
+<template>
+  <main class="w-screen h-screen flex justify-center items-center">
+    <div class="w-1/2 h-1/2">
+      <FileUploader />
+    </div>
+  </main>  
+</template>
+
